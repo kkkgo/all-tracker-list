9e26403b4434cc02f39944c711387c254d818225
kissshot <4530897+kkkgo@users.noreply.github.com>
2023-07-04 23:57:31 +0800

    trackerslist

--- update_data.sh
+++ update_data.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 mkdir download
 touch download/temp.txt
-touch trackerlist.txt
+touch trackerslist.txt
 while read -r url; do
     curl -s "$url" >>download/temp.txt
     echo "" >>download/temp.txt
 done <url.txt
-cat trackerlist.txt >>download/temp.txt
-sort -u download/temp.txt | grep "." >trackerlist.txt
+cat trackerslist.txt >>download/temp.txt
+sort -u download/temp.txt | grep "." >trackerslist.txt
 rm -rf download
-sha256sum trackerlist.txt | cut -d" " -f1 >trackerlist.txt.sha256sum
-rm trackerlist.txt.xz
-xz -9 -k -e trackerlist.txt
-sha256sum trackerlist.txt.xz | cut -d" " -f1 >trackerlist.txt.xz.sha256sum
+sha256sum trackerslist.txt | cut -d" " -f1 >trackerslist.txt.sha256sum
+rm trackerslist.txt.xz
+xz -9 -k -e trackerslist.txt
+sha256sum trackerslist.txt.xz | cut -d" " -f1 >trackerslist.txt.xz.sha256sum
