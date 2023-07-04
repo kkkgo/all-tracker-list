085d549ee46ec65b55c490a9ec87a675babf5274
kissshot <4530897+kkkgo@users.noreply.github.com>
2023-07-04 23:09:26 +0800

    init

---
+++ update_data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir download
+touch download/temp.txt
+touch trackerlist.txt
+while read -r url; do
+    curl -s "$url" >>download/temp.txt
+    echo "" >>download/temp.txt
+done <url.txt
+cat trackerlist.txt >>download/temp.txt
+sort -u download/temp.txt >trackerlist.txt
+rm -rf download
+sha256sum trackerlist.txt | cut -d" " -f1 >trackerlist.txt.sha256sum
