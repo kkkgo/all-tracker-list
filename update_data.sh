cf0e706097f026f7cbbb23c117d548941926421d
kissshot <4530897+kkkgo@users.noreply.github.com>
2023-07-04 23:21:45 +0800

    xz

--- update_data.sh
+++ update_data.sh
@@ -10,3 +10,4 @@ cat trackerlist.txt >>download/temp.txt
 sort -u download/temp.txt | grep "." >trackerlist.txt
 rm -rf download
 sha256sum trackerlist.txt | cut -d" " -f1 >trackerlist.txt.sha256sum
+xz -9 -k -e trackerlist.txt
