4d97c0ddb54521e4be83dd3be860635b1c25f3f5
kissshot <4530897+kkkgo@users.noreply.github.com>
2023-07-04 23:24:00 +0800

    Update update_data.sh

--- update_data.sh
+++ update_data.sh
@@ -10,4 +10,6 @@ cat trackerlist.txt >>download/temp.txt
 sort -u download/temp.txt | grep "." >trackerlist.txt
 rm -rf download
 sha256sum trackerlist.txt | cut -d" " -f1 >trackerlist.txt.sha256sum
+rm trackerlist.txt.xz
 xz -9 -k -e trackerlist.txt
+sha256sum trackerlist.txt.xz | cut -d" " -f1 >trackerlist.txt.xz.sha256sum
