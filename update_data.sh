287602688070dbf00e491727902d569d160631f5
kissshot <4530897+kkkgo@users.noreply.github.com>
2023-07-07 01:20:47 +0800

    Update update_data.sh

--- update_data.sh
+++ update_data.sh
@@ -3,11 +3,16 @@ mkdir download
 touch download/temp.txt
 touch trackerslist.txt
 while read -r url; do
-    curl -s "$url" >>download/temp.txt
+    echo "" >>download/temp.txt
+    TEST=$(curl -s "$url")
+    if [ "$?" = "0" ] && echo $TEST | grep -q "://"; then
+        echo $TEST | grep "://" >>download/temp.txt
+    fi
     echo "" >>download/temp.txt
 done <url.txt
 cat trackerslist.txt >>download/temp.txt
-sort -u download/temp.txt | grep "." >trackerslist.txt
+echo "" >>download/temp.txt
+sort -u download/temp.txt | grep "://" >trackerslist.txt
 rm -rf download
 sha256sum trackerslist.txt | cut -d" " -f1 >trackerslist.txt.sha256sum
 rm trackerslist.txt.xz
